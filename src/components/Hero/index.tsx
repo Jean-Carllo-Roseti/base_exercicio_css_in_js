e818e205b741ac89f1d2e723d0b130e8ce7b0732
jean carllo <jeanrosete04@gmail.com>
2024-01-22 18:47:56 -0300

    finalizado o componente Hero.

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,13 @@
-import styles from './Hero.module.css'
+import { HeroForm, HeroTitle, HeroDivForm } from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
+  <HeroForm>
+    <HeroDivForm>
+      <HeroTitle>
         As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+      </HeroTitle>
+    </HeroDivForm>
+  </HeroForm>
 )
 
 export default Hero
